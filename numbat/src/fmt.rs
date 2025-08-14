85e1f426305fa0232fc303a17690af9513e0e620
Mads Møller Jensen <mads1606@gmail.com>
2025-08-14 22:36:55 +0200

    Begin builder implementation

--- numbat/src/fmt.rs
+++ numbat/src/fmt.rs
@@ -138,3 +138,94 @@ impl Generator {
         self.buffer.push_str(&INDENT.repeat(self.indent));
     }
 }
+
+
+struct Builder<'a> {
+    id: usize,
+    source: &'a str,
+}
+
+impl Builder<'_> {
+    fn new<'a>(source: &'a str) -> Builder<'a> {
+        Builder { id: 0, source }
+    }
+
+    fn build<'a>(&'a mut self, ast: Vec<Statement<'a>>) -> FormatNode {
+        let format_nodes = ast.iter().map(|n| self.build_statement(n)).collect();
+        FormatNode::Nodes(format_nodes)
+    }
+
+    fn new_id(&mut self) -> usize {
+        self.id += 1;
+        self.id
+    }
+
+    fn build_statement<'a>(&mut self, node: &Statement<'a>) -> FormatNode {
+        match node {
+            Statement::Expression(expr) => self.build_expression(expr),
+            Statement::DefineVariable(define_variable) => todo!(),
+            Statement::DefineFunction {
+                function_name_span,
+                function_name,
+                type_parameters,
+                parameters,
+                body,
+                local_variables,
+                return_type_annotation,
+                decorators,
+            } => todo!(),
+            Statement::DefineDimension(span, _, type_expressions) => todo!(),
+            Statement::DefineBaseUnit(span, _, type_expression, decorators) => todo!(),
+            Statement::DefineDerivedUnit {
+                identifier_span,
+                identifier,
+                expr,
+                type_annotation_span,
+                type_annotation,
+                decorators,
+            } => todo!(),
+            Statement::ProcedureCall(span, procedure_kind, expressions) => todo!(),
+            Statement::ModuleImport(span, module_path_borrowed) => todo!(),
+            Statement::DefineStruct {
+                struct_name_span,
+                struct_name,
+                fields,
+            } => todo!(),
+        }
+    }
+
+    fn build_expression<'a>(&'a mut self, expr: &Expression<'a>) -> FormatNode {
+        match expr {
+            Expression::Scalar(_, number) => {
+                FormatNode::Text(number.clone().pretty_print().to_string())
+            }
+            Expression::Identifier(span, _) => todo!(),
+            Expression::UnitIdentifier(span, prefix, compact_string, compact_string1) => todo!(),
+            Expression::TypedHole(span) => todo!(),
+            Expression::UnaryOperator { op, expr, span_op } => todo!(),
+            Expression::BinaryOperator {
+                op,
+                lhs,
+                rhs,
+                span_op,
+            } => todo!(),
+            Expression::FunctionCall(span, span1, expression, expressions) => todo!(),
+            Expression::Boolean(span, _) => todo!(),
+            Expression::String(span, string_parts) => todo!(),
+            Expression::Condition(span, expression, expression1, expression2) => todo!(),
+            Expression::InstantiateStruct {
+                full_span,
+                ident_span,
+                name,
+                fields,
+            } => todo!(),
+            Expression::AccessField(span, span1, expression, _) => todo!(),
+            Expression::List(span, expressions) => todo!(),
+        }
+    }
+}
+
+pub fn fmt(source: &str, ast: Vec<Statement>, max_width: usize) -> String {
+    let format_tree = Builder::new(source).build(ast);
+    Generator::new(max_width).generate(format_tree)
+}
